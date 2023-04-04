752af5e75e16cc64c2ecee9ee03f64ea6e38996d
Gentrit Qorri <gentrit5.qorri@gmail.com>
2023-04-04 22:20:19 +0200

    first commit

---
+++ src/robots.js
@@ -0,0 +1,75 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Fjolla Zhushi',
+      username: 'Fjolla',
+      email: 'Fjolla.zh@gmail.com'
+    },
+    {
+      id: 7,
+      name: 'Gentrit Qorri',
+      username: 'Gentt',
+      email: 'Gentt@gmail.com'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 2,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 11,
+      name: 'Robert Berisha',
+      username: 'Berti',
+      email: 'Rober.b@outlook.com'
+    },
+    {
+      id: 12,
+      name: 'Domminik Wiss',
+      username: 'Domm',
+      email: 'Rey.Padberg@karina.biz'
+    }
+  ];
+  
